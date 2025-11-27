b6207dff6cc748d4157810990f4bcb5fe89420c4
letYuchan <142238082+letYuchan@users.noreply.github.com>
2025-11-27 20:31:42 +0900

    feat: 프로젝트 내용 정리 및 추가 (#1)

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -6,31 +6,103 @@ const LandingPage = () => {
       <h1 className="font-title font-extrabold text-5xl text-on-surface-accent my-52">
         Welcome to My World
       </h1>
-      <section>
-        <h2>Introduce myself</h2>
-        <nav>
-          <ul>
-            <li>Passion for learning and accumulated knowledge</li>
-            <li>Projects completed with persistence</li>
-            <li>Continuous challenges and growth</li>
-          </ul>
-        </nav>
-        <section>
-          <h2>Introduce my project</h2>
-          <nav>
-            <ul>
-              <CardSection
-                title="Online Card Blind Date Service"
-                description1="First project"
-                description2="Festival events online"
-                description3="VanillaJS·CSS·HTML"
-                maxW={500}
-                height={250}
-                position="left"
-              />
-            </ul>
-          </nav>
-        </section>
+      <section className="w-full max-w-7xl">
+        <h2 className="font-title font-semibold text-3xl text-on-surface-primary text-center mb-40">
+          Introduce myself
+        </h2>
+        <ul>
+          <li className="font-normal text-xl text-on-surface-accent text-left mb-20">
+            Passion for learning and accumulated knowledge
+          </li>
+          <li className="font-normal text-xl text-on-surface-accent text-right mb-20">
+            Projects completed with persistence
+          </li>
+          <li className="font-normal text-xl text-on-surface-accent text-left mb-20">
+            Continuous challenges and growth
+          </li>
+        </ul>
+        <div className="h-1 w-full bg-line-primary my-52" />
+      </section>
+      {/* 프로젝트 소개 */}
+      <section className="w-full max-w-7xl">
+        <h2 className="font-title font-semibold text-3xl text-on-surface-primary text-center mb-40">
+          Introduce my projects
+        </h2>
+        <ul className="grid grid-cols-3 grid-row-auto gap-10">
+          <li>
+            <CardSection
+              title="First Project, Digital Card Blind Date Service"
+              description1="A blind dating service for college students"
+              description2="Recommended draw and random draw"
+              description3="Dating Chatbot Simulation"
+              stack="VanillaJS·CSS·HTML"
+              maxW={450}
+              height={350}
+              position="left"
+            />
+          </li>
+          <li>
+            <CardSection
+              title="Renewal Web Project of Date Service, SSUMPICK"
+              description1="Deploy as a real service"
+              description2="About 200 users per week"
+              description3="Personalization enhanced with similar animal figures and point colors"
+              stack="React·TailwindCSS·Zustand·Tanstack Query&Table"
+              maxW={450}
+              height={350}
+              position="left"
+            />
+          </li>
+          <li>
+            <CardSection
+              title="Serverless Technical Blog platform, Velogit"
+              description1="Provides powerful writing automation"
+              description2="A high-quality design system with 16 themes"
+              description3="Commenting is possible without a server"
+              stack="React·TailwindCSS·Zustand"
+              maxW={450}
+              height={350}
+              position="left"
+            />
+          </li>
+          <li>
+            <CardSection
+              title="Golf course control system, TRINITY Club"
+              description1="Responsible for linear course map"
+              description2="Real-time coordinates data handling, Using Own Design System"
+              description3="Connect player, caddy, hole, and golf cart data"
+              stack="React·TailwindCSS·Zustand·TanstackQuery"
+              maxW={450}
+              height={350}
+              position="left"
+            />
+          </li>
+          <li>
+            <CardSection
+              title="B2B automated quotation system, ETEVERS"
+              description1="Display complex quote status in UI"
+              description2="All UI publishing within the deadline"
+              description3="B2B responsive and complex UI design experience"
+              stack="React·TailwindCSS·Zustand·Tanstack Query&Table"
+              maxW={450}
+              height={350}
+              position="left"
+            />
+          </li>
+          <li>
+            <CardSection
+              title="Integrated route navigation solution, Ddareungi Map"
+              description1="Providing walk-bike-walk route for Seoul's public bicycles"
+              description2="Real-time voice navigation"
+              description3="design Webview-RN data flow"
+              stack="React Native·TailwindCSS·Context API·Zustand·Tanstack Query"
+              maxW={450}
+              height={350}
+              position="left"
+            />
+          </li>
+        </ul>
+        <div className="h-1 w-full bg-line-primary my-52" />
       </section>
     </main>
   );
--- src/shared/components/CardSection.tsx
+++ src/shared/components/CardSection.tsx
@@ -3,6 +3,7 @@ interface CardSectionProps {
   description1: string;
   description2: string;
   description3: string;
+  stack: string;
   maxW: number;
   height: number;
   position: "left" | "center" | "right";
@@ -13,34 +14,31 @@ const CardSection = ({
   description1,
   description2,
   description3,
+  stack,
   maxW,
   height,
   position,
 }: CardSectionProps) => {
   return (
     <div
-      className="w-full bg-surface-secondary border-line-primary px-4 py-2 border rounded-xl flex flex-col justify-center items-center hover:border-line-accent hover:shadow-soft"
-      style={{ maxWidth: maxW, height: height }}
+      className="w-full bg-surface-secondary border-line-primary px-4 py-6 border rounded-xl flex flex-col justify-start hover:border-line-accent hover:shadow-soft"
+      style={{ maxWidth: maxW, height }}
     >
       <h3
-        className={`font-title font-semibold text-on-surface-accent text-2xl text-${position} w-full mb-10`}
+        className={`font-title font-semibold text-on-surface-accent text-2xl w-full mb-10 text-${position}`}
       >
         {title}
       </h3>
-      <p
-        className={`font-normal text-on-surface-primary text-xl text-${position} w-full mb-2`}
+
+      <ul
+        className={`list-disc list-outside pl-6 space-y-2 text-on-surface-primary text-${position} mb-10`}
       >
-        · {description1}
-      </p>
-      <p
-        className={`font-normal text-on-surface-primary text-xl text-${position} w-full mb-2`}
-      >
-        · {description2}
-      </p>
-      <p
-        className={`font-normal text-on-surface-primary text-xl text-${position} w-full mb-2`}
-      >
-        · {description3}
+        <li className="font-normal text-xl">{description1}</li>
+        <li className="font-normal text-xl">{description2}</li>
+        <li className="font-normal text-xl">{description3}</li>
+      </ul>
+      <p className={`font-light text-xs text-on-surface-muted text-center`}>
+        Stack: {stack}
       </p>
     </div>
   );
