4f3e1cca714522a32d33331fc0d4e191d357ad81
letYuchan <142238082+letYuchan@users.noreply.github.com>
2025-11-28 18:21:59 +0900

    feat: Circular 갤러리를 통한 homePage 기본 뼈대 구현 (#3)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,15 @@
-import CardSwap, { Card } from "@/pages/home/components/Card";
 import NebulaLoader from "@/shared/components/NebulaLoader";
 import { useEffect, useState } from "react";
-import card1 from "@/assets/imgs/다운로드.jpeg";
+import card1 from "@/assets/imgs/digitalCardDateService.png";
+import card2 from "@/assets/imgs/ssumpick.png";
+import card3 from "@/assets/imgs/trinity.png";
+import card4 from "@/assets/imgs/etevers.png";
+import card5 from "@/assets/imgs/cloody.png";
+import card6 from "@/assets/imgs/velogit.png";
+import card7 from "@/assets/imgs/ddareungiMap.png";
+import background from "@/assets/imgs/homeBg.png";
+import CircularGallery from "@/pages/home/CircularGallery";
+import SoftRectParticles from "@/shared/components/SoftRectParticles";
 
 const HomePage = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -17,32 +25,31 @@ const HomePage = () => {
   }
 
   return (
-    <main className="w-full h-screen bg-surface-primary flex justify-center items-center">
-      <div style={{ height: "600px", position: "relative" }}>
-        <CardSwap
-          cardDistance={60}
-          verticalDistance={70}
-          delay={5000}
-          pauseOnHover={false}
-        >
-          <Card>
-            <h3>Card 1</h3>
-            <img
-              src={card1}
-              alt=""
-              className="w-full h-full object-cover rounded-xl"
-            />
-          </Card>
-          <Card>
-            <h3>Card 2</h3>
-            <p>Your content here</p>
-          </Card>
-          <Card>
-            <h3>Card 3</h3>
-            <p>Your content here</p>
-          </Card>
-        </CardSwap>
-      </div>
+    <main
+      className="w-full min-h-screen flex flex-col justify-center items-center bg-surface-primary relative bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <section className="w-full max-w-7xl px-4 flex flex-col justify-center items-center relative h-[600px]">
+        <h2 className="font-title font-semibold text-4xl text-on-surface-primary text-center mb-16">
+          Choose my recent works
+        </h2>
+        <CircularGallery
+          bend={3}
+          textColor="#E7E4F5"
+          borderRadius={0.05}
+          scrollEase={0.02}
+          items={[
+            { image: card1, text: "Digital Card Date Service" },
+            { image: card2, text: "SSUMPICK" },
+            { image: card3, text: "TRINITY" },
+            { image: card4, text: "ETEVERS" },
+            { image: card5, text: "CLOODY" },
+            { image: card6, text: "VELOGIT" },
+            { image: card7, text: "DDAREUNGI MAP" },
+          ]}
+        />
+      </section>
+      <SoftRectParticles />
     </main>
   );
 };
